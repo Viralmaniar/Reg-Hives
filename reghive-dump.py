2fd4b6d9b99f596298cccc7c2315134b316fe0da
Viral Maniar <maniar.viral@gmail.com>
2017-04-06 23:51:15 +1000

    Create reghive-dump.py
    general idea about dumping registry hives. Need more ideas...

---
+++ reghive-dump.py
@@ -0,0 +1,25 @@
+# Import tkinter library
+from tkinter import *
+
+# Define empty outer window
+window=Tk()
+
+
+def sam():
+    print("debug")
+
+# Creating button for the SAM key
+b1 = Button(window, text="Save SAM",command=sam)
+b1.grid(row=0,column=1)
+
+# Creating button for the SECURITY key
+b2 = Button(window, text="Save SECURITY")
+b2.grid(row=1,column=1)
+
+# Creating button for the SYSTEM key
+b3 = Button(window, text="Save SYSTEM")
+b3.grid(row=2,column=1)
+
+# Adding widgets to the window
+t1=Text(window)
+t1.grid(row=1,column=2)
